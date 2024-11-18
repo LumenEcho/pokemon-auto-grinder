88e2a405e9dc5f112629f12735b89ee330bc7b55
LumenEcho <pestilliva@gmail.com>
2024-11-17 20:42:46 -0600

    initialize commit

---
+++ pokemonGrinder.py
@@ -0,0 +1,11 @@
+import keyboard
+import pyautogui
+
+pyautogui.FAILSAFE = True
+pyautogui.PAUSE = 1
+
+keyboard.wait('-')
+while (not keyboard.is_pressed('shift')):
+    pyautogui.keyDown('s')
+    pyautogui.keyDown('w')
+    pyautogui.keyDown('enter')
